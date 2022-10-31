23a1df6a33cb68ec6dda48b2aa6a0f39c8d3fd72
BoYoung00 <101164065+BoYoung00@users.noreply.github.com>
2022-10-31 22:51:39 +0900

    백엔드 공부 복습설명

--- src/main/java/Dao/boardDao.java
+++ src/main/java/Dao/boardDao.java
@@ -9,47 +9,55 @@ import java.sql.ResultSet;
 
 public class boardDao {
     //jdbc:mysql://localhost:3306/bydbst
-    //중요 다오 순서
-    //1단계 import java.sql.*
-    //2단계 드라이버를 로드한다.
-    //3단계 커넥션 객체 생성//데이터베이스 연결
-    //4단계 Stat먼트 객체 생성//sql문장 수행
-    //SQL문에 결과물이 있다면 resultSet 생성//결과값 가져오기
+    //중요 dao 순서
+    //1단계 import java.sql.* : sql 일단 연동
+    //2단계 드라이버를 로드한다. : 본인 컴퓨터에 깔려있는 버전
+    //3단계 Connection 객체 생성 : 데이터베이스 연결(url, user, password 필요)
+    //4단계 Statement 객체 생성//sql문장 수행
+    //SQL문에 결과물이 있다면 resultSet 생성 //결과값 가져오기
     //객체 닫아주기 (중요)
 
     private static String url="jdbc:mysql://localhost:3306/bydbst";
     private static String user="root";
     private static String password="rlaqhdud2@";
 
+    //추가
     public int addUser(boardDto boardDto){
-        int inserCount=0;
+        int inserCount=0; //추가가 됐는지 확인하는 용도
 
-        try {
+        try { //sql 드라이버 로드
             Class.forName("com.mysql.cj.jdbc.Driver");
         }catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
+        //sql문장 미리 저장해놓기
         String sql = "insert into board(title, content) values (?,?)";
 
+        //Connection : 데이터베이스 연결, preparedStatement : sql 문장 실행 역할
         try(Connection conn = DriverManager.getConnection(url, user, password);
             PreparedStatement ps = conn.prepareStatement(sql);) {
 
             ps.setString(1, boardDto.getTitle());
             ps.setString(2, boardDto.getContent());
 
-            inserCount = ps.executeUpdate(); //★★★★★★
+            //★★★★★★ executeUpdate : int값 반환, ExecuteQuery : 객체 값 반환, 반환 : 블리언 값 반환
+            inserCount = ps.executeUpdate();
         }catch (Exception e) {
             e.printStackTrace();
         }
         return inserCount;
     }
 
+    //찾기
     public boardDto getDto(String id) {
-        boardDto boardDto = null;
+        //초기화
+        boardDto boardDto = null; //객체 연결
         Connection conn = null; //데이터베이스 연결
 
         PreparedStatement ps=null; //sql문장
         ResultSet rs = null; //값으로 뭔갈 해야할 때 사용
+        //ResultSet : 결과 값 저장, 행 단위 불러올 수 있음, 한 행 값 타입 지정 가능
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -59,29 +67,32 @@ public class boardDao {
             ps.setString(1,id);
 
             rs=ps.executeQuery();
-            if (rs.next()){
-                String title=rs.getString(1);
-                String content=rs.getString(2);
-                boardDto=new boardDto(title, content);
+
+            if (rs.next()){ //rs값이 있다면
+                String title = rs.getString(1); //1행
+                String content = rs.getString(2); //2행
+                boardDto = new boardDto(title, content);
             }
+
         }catch (Exception e){
             e.printStackTrace();
+
         }finally { //무조건 한 번 실행 시키기
-            if (rs != null) {
+            if (rs != null) { //Resultset 값 없다면
                 try {
-                    rs.close();
+                    rs.close(); //닫아라
                 }catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-            if (ps != null) {
+            if (ps != null) { //sql문장 없다면
                 try {
                     ps.close();
                 }catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-            if (conn != null) {
+            if (conn != null) { //DB 드라이버 없다면
                 try {
                     conn.close();
                 }catch (Exception e) {
@@ -90,22 +101,23 @@ public class boardDao {
             }
         }
         return boardDto;
-
     }
+
+    //삭제
     public int deleteTest(String title) {
         int deleteCount = 0;
 
-        Connection conn=null;
+        Connection conn = null;
 
-        PreparedStatement ps=null;
+        PreparedStatement ps = null;
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             conn = DriverManager.getConnection(url, user, password);
-            String sql="delete from board where title=?";
-            ps=conn.prepareStatement(sql);
+            String sql = "delete from board where title=?";
+            ps = conn.prepareStatement(sql);
             ps.setString(1, title);
-            deleteCount=ps.executeUpdate();
+            deleteCount = ps.executeUpdate(); //int값 반환
 
         }catch (Exception e) {
             e.printStackTrace();
@@ -130,11 +142,12 @@ public class boardDao {
         return deleteCount;
     }
 
+    //수정
     public int update(boardDto boardDto) {
         int updateCount=0;
 
-        Connection conn=null;
-        PreparedStatement ps=null;
+        Connection conn = null;
+        PreparedStatement ps = null;
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
--- src/main/java/Test/Exam.java
+++ src/main/java/Test/Exam.java
@@ -19,6 +19,8 @@ public class Exam {
 //        boardDto dto=dao.getDto("타이틀입니다1");
 //        System.out.println(dto);
 
+
+        //수정
         String title="타이틀입니다2";
         String content="타이틀입니다 수정본";
 
@@ -30,28 +32,33 @@ public class Exam {
         System.out.println(updatecount);
 
     }
+    //추가
     public static void adddata(String head, String body){
         String title=head;
         String content=body;
 
         boardDto dto = new boardDto(title, content);
-
         boardDao dao=new boardDao();
+
         int insertCount=dao.addUser(dto);
 
         System.out.println(insertCount);
     }
 
+    //찾기
     public  static void getdata(){
         boardDao dao=new boardDao();
         boardDto dto=dao.getDto("타이틀입니다2");
+
         System.out.println(dto);
 
     }
 
+    //삭제
     public  static void delete(){
         String title="타이틀입니다";
 
+        //왜 삭제만 dao 부름?
         boardDao boardDao = new boardDao();
         int deleteCount = boardDao.deleteTest(title);
 
